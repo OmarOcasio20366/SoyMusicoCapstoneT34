752ced7bca9a272dba2413b8594634b33868ae27
OmarOcasio <omancasio@gmail.com>
2024-10-15 19:40:46 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './globals.css'
+
+const App = () => {
+  return (
+    
+
+    <h1 className='text-3x1 font-bold underline'>Welcome to Musician Social App</h1>
+
+  )
+}
+
+export default App
