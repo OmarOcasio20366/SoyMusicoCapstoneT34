360e4a57dc7189449f07583fdcdb8cdd049cf8d9
OmarOcasio <omancasio@gmail.com>
2024-10-17 19:08:15 -0400

    Fixed the project ID and endpoint in the environment file

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -1,18 +1,16 @@
 import { Client, Account, Databases, Storage, Avatars } from "appwrite"
 
-// export const appwriteConfig = {
+export const appwriteConfig = {
 
-//     projectID: import.meta.env.VITE_APPWRITE_PROJECT_ID,
-//     url: import.meta.env.VITE_APPWRITE_URL,
+    projectID: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+    url: import.meta.env.VITE_APPWRITE_URL,
 
-// }
+}
 
 export const client = new Client();
 
-// client.setProject(appwriteConfig.projectID)
-// client.setEndpoint(appwriteConfig.url)
-client.setProject("670b012c002f2000d544")
-client.setEndpoint("https://cloud.appwrite.io/v1")
+client.setProject(appwriteConfig.projectID)
+client.setEndpoint(appwriteConfig.url)
 
 export const account = new Account(client);
 export const databases = new Databases(client);
